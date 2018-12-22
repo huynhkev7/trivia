8bea4ab36c293755498878ba54575fe7438e2964
Kevin Huynh <kevdihuynh@gmail.com>
2018-12-22 12:48:41 -0800

    a

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,21 @@
 export const environment = {
   production: true,
-  firebase : {
-    apiKey: 'AIzaSyB4Vy-3nGM6KyKvEemtvDOtc8hlcSVaKxI',
-    authDomain: 'trivia-poll-game.firebaseapp.com',
-    databaseURL: 'https://trivia-poll-game.firebaseio.com',
-    projectId: 'trivia-poll-game',
-    storageBucket: 'trivia-poll-game.appspot.com',
-    messagingSenderId: '356882557715'
-  }
+  // firebase : {
+  //   apiKey: 'AIzaSyB4Vy-3nGM6KyKvEemtvDOtc8hlcSVaKxI',
+  //   authDomain: 'trivia-poll-game.firebaseapp.com',
+  //   databaseURL: 'https://trivia-poll-game.firebaseio.com',
+  //   projectId: 'trivia-poll-game',
+  //   storageBucket: 'trivia-poll-game.appspot.com',
+  //   messagingSenderId: '356882557715'
+  // }
+
+    // Initialize Shelley Firebase
+    firebase : {
+      apiKey: 'AIzaSyBRW0N9YU0QlH82dzMIl7fdu5jiKGuhrMs',
+      authDomain: 'trivia-game-shelley.firebaseapp.com',
+      databaseURL: 'https://trivia-game-shelley.firebaseio.com',
+      projectId: 'trivia-game-shelley',
+      storageBucket: 'trivia-game-shelley.appspot.com',
+      messagingSenderId: '882625626245'
+    }
 };
